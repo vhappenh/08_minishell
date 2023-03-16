d995505da976542c5ac5b8b148f5d3695c0f4fba
Valerian Happenhofer <vhappenh@c3r9p5.42vienna.com>
2023-03-16 15:06:01 +0100

    implemented headers to headerfile

--- src/vr.h
+++ src/vr.h
@@ -6,7 +6,7 @@
 /*   By: vhappenh <vhappenh@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 14:11:19 by vhappenh          #+#    #+#             */
-/*   Updated: 2023/03/16 14:37:04 by vhappenh         ###   ########.fr       */
+/*   Updated: 2023/03/16 15:02:43 by vhappenh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 # include "libft_ultra/libft.h"
 # include <stdio.h>
 # include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 typedef struct s_cmdline
 {
@@ -24,7 +26,7 @@ typedef struct s_cmdline
 	char	*nxt_op;
 }	t_cmdline;
 
-int		input_parse(t_cmdline **todo);
+int		input_parse(t_cmdline **todo, char *input);
 int		execute(t_cmdline **todo, char **envp);
 int		ft_free(t_cmdline **todo);
 int		ft_doublejoin(char *split_path, char **paths, t_cmdline **todo);
