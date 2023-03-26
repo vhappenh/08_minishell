533cfd95ac6693653141c6dd85eb168d69d0be72
Valerian Happenhofer <vhappenh@c2r7p6.42vienna.com>
2023-03-26 13:04:55 +0200

    fixed the empty cmd

--- src/utils1.c
+++ src/utils1.c
@@ -6,7 +6,7 @@
 /*   By: vhappenh <vhappenh@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 14:09:14 by vhappenh          #+#    #+#             */
-/*   Updated: 2023/03/18 14:14:19 by vhappenh         ###   ########.fr       */
+/*   Updated: 2023/03/26 13:04:26 by vhappenh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,16 @@ int	ft_doublejoin(char *split_path, char **path, t_cmdline **todo)
 	temp1 = ft_strjoin(split_path, "/");
 	if (temp1 == NULL)
 		return (1);
+	if (!todo[0]->cmd[0])
+		return (2);
 	temp2 = ft_strjoin(temp1, todo[0]->cmd[0]);
 	free (temp1);
 	if (temp2 == NULL)
-		return (2);
+		return (3);
 	if (access(temp2, X_OK))
 	{
 		free (temp2);
-		return (3);
+		return (4);
 	}
 	*path = ft_strdup(temp2);
 	free (temp2);
