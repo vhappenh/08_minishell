6288989e1e7969995043456ead9edd4048f42e42
Raphael Rupp <rrupp@c3r9p6.42vienna.com>
2023-03-28 13:56:45 +0200

    I norminetted a bit!

--- src/utils_token.c
+++ src/utils_token.c
@@ -6,7 +6,7 @@
 /*   By: rrupp <rrupp@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 09:42:16 by rrupp             #+#    #+#             */
-/*   Updated: 2023/03/28 11:51:16 by rrupp            ###   ########.fr       */
+/*   Updated: 2023/03/28 13:37:03 by rrupp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,21 @@ int	ft_count_token(char *input)
 	return (tokens);
 }
 
+static int	ft_jump_quots(char *input, int i)
+{
+	char	c;
+
+	c = input[i++];
+	while (input[i] || input[i] != c)
+		i++;
+	return (i);
+}
+
 char	*ft_get_token(char *input, int check)
 {
 	static int	i;
 	int			j;
 	int			k;
-	char		c;
 	char		*token;
 
 	if (check == 0)
@@ -40,19 +49,16 @@ char	*ft_get_token(char *input, int check)
 	while (input[i])
 	{
 		if (input[i] == '\'' || input[i] == '"')
-		{
-			c = input[i++];
-			while (input[i] || input[i] != c)
-				i++;
-		}
+			i = ft_jump_quots(input, i);
 		if (input[i] == '|')
 			break ;
 		i++;
 	}
 	token = ft_calloc(i - j + 1, sizeof(char));
+	if (token == NULL)
+		return (NULL);
 	while (j < i)
 		token[k++] = input[j++];
-	//if (token[i] == '|')
 	i++;
 	return (token);
 }
