75a1ce6f9371b0321a853d7e1361801e71ec6d29
Valerian Happenhofer <vhappenh@c4r5p6.42vienna.com>
2023-04-20 21:31:09 +0200

    late night push. I guess we did something before lol

--- src/built_ins3.c
+++ src/built_ins3.c
@@ -6,50 +6,73 @@
 /*   By: vhappenh <vhappenh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 14:15:40 by vhappenh          #+#    #+#             */
-/*   Updated: 2023/04/20 13:14:31 by vhappenh         ###   ########.fr       */
+/*   Updated: 2023/04/20 15:10:13 by vhappenh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vr.h"
 
-static int	ft_export_argument(t_cmdline *todo, t_envlst *env)
+static int	ft_search_and_replace(char *todocmd, t_envlst *env)
 {
-	char		*temp1;
-	char		*temp2;
+	char	*temp;
+	
+	(void) env;
+	temp = ft_strncopy(todocmd, ft_search_char(todocmd, '=') - 1);
+	if (temp == NULL)
+		return (1);
+	while (env)
+	{
+		if (temp == env->evar)
+		{
+			free (env->cont);
+			env->cont = ft_strdup(todocmd + ft_search_char(todocmd, '=') + 1);
+			if (env->cont == NULL)
+			{
+				free (temp);
+				return (2);
+			}
+		}
+		env = env->next;
+	}
+	free (temp);
+	return (0);
+}
+
+static int	ft_export_arg_execute(char *str1, char *str2, t_envlst **temp)
+{
+	char	*temp1;
+	char	*temp2;
+
+	temp1 = ft_strncopy(str1, ft_search_char(str1, '='));
+	if (temp1 == NULL)
+		return (1);
+	temp2 = ft_strdup(str2);
+	if (temp2 == NULL)
+		return (1);
+	*temp = ft_lstnew_minishell(ft_strdup(temp1), ft_strdup(temp2));
+	free (temp1);
+	free (temp2);
+	if (*temp == NULL)
+		return (3);
+	return (0);
+}
+
+static int	ft_export_argument(char *todocmd, t_envlst *env)
+{
 	t_envlst	*templst;
 
-	if (ft_strchr(todo->cmd[1], '='))
+	if (ft_strchr(todocmd, '='))
 	{	
-		if (!todo->cmd[1][ft_search_char(todo->cmd[1], '=') + 1])
+		if (!todocmd[ft_search_char(todocmd, '=') + 1])
 		{
-			temp1 = ft_strncopy(todo->cmd[1],
-					ft_search_char(todo->cmd[1], '='));
-			if (temp1 == NULL)
+			if (ft_export_arg_execute(todocmd, "", &templst))
 				return (1);
-			temp2 = ft_strdup("");
-			if (temp2 == NULL)
-				return (1);
-			templst = ft_lstnew_minishell(ft_strdup(temp1), ft_strdup(temp2));
-			free (temp1);
-			free (temp2);
-			if (templst == NULL)
-				return (3);
 		}
 		else
 		{		
-			temp1 = ft_strncopy(todo->cmd[1],
-					ft_search_char(todo->cmd[1], '='));
-			if (temp1 == NULL)
-				return (1);
-			temp2 = ft_strdup(todo->cmd[1]
-					+ ft_search_char(todo->cmd[1], '=') + 1);
-			if (temp2 == NULL)
-				return (1);
-			templst = ft_lstnew_minishell(ft_strdup(temp1), ft_strdup(temp2));
-			free (temp1);
-			free (temp2);
-			if (templst == NULL)
-				return (3);
+			if (ft_export_arg_execute(todocmd, ft_strdup(todocmd
+						+ ft_search_char(todocmd, '=') + 1), &templst))
+				return (2);
 		}
 		ft_lstadd_back_minishell(&env, templst);
 	}
@@ -72,15 +95,21 @@ static int	ft_export_no_argument(t_cmdline *todo, t_envlst *env)
 
 int	ft_export(t_cmdline *todo, t_envlst *env)
 {
-	if (todo->cmd[1])
+	int	i;
+
+	i = 0;
+	while (todo->cmd[++i])
 	{
-		if (ft_export_argument(todo, env))
-			return (1);
-	}
-	else
-	{
-		if (ft_export_no_argument(todo, env))
-			return (2);
+		if (todo->cmd[i])
+		{
+			if (ft_export_argument(todo->cmd[i], env))
+				return (1);
+		}
+		else
+		{
+			if (ft_export_no_argument(todo, env))
+				return (2);
+		}
 	}
 	return (0);
 }
