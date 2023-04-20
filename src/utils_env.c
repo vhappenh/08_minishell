f41db20b68616dbe692bc0954377a2087c0a0b1c
Raphael Rupp <rrupp@c4r4p2.42vienna.com>
2023-04-20 10:30:14 +0200

    I norminetted this stuff :)

--- src/utils_env.c
+++ src/utils_env.c
@@ -6,22 +6,11 @@
 /*   By: rrupp <rrupp@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 11:32:10 by rrupp             #+#    #+#             */
-/*   Updated: 2023/04/19 18:41:49 by rrupp            ###   ########.fr       */
+/*   Updated: 2023/04/20 10:27:45 by rrupp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vr.h"
-
-static int	ft_freeenv(char *str1, char *str2, char *str3)
-{
-	if (str1)
-		free(str1);
-	if (str2)
-		free(str2);
-	if (str3)
-		free(str3);
-	return (1);
-}
 
 char	*ft_search_return_env(char *env, t_envlst *enviroment)
 {
@@ -66,7 +55,8 @@ static int	ft_get_env(char **str, int i, int j, t_envlst *enviroment)
 	char	*tmp;
 	char	*result;
 
-	while ((*str)[j] && (*str)[j] != ' ' && (*str)[j] != '"')
+	while ((*str)[j] && (*str)[j] != ' ' && \
+		(*str)[j] != '"' && (*str)[j] != '\'')
 		j++;
 	env = ft_strncopy((&(*str)[i] + 1), j - i - 1);
 	if (env == NULL)
@@ -79,25 +69,41 @@ static int	ft_get_env(char **str, int i, int j, t_envlst *enviroment)
 	else
 		tmp = ft_strdup("");
 	if (tmp == NULL)
-		return (ft_freeenv(env, NULL, NULL));
+		return (ft_free_threestr(env, NULL, NULL));
 	result = ft_doublejoin(tmp, env, &(*str)[j]);
 	if (result == NULL)
-		return (ft_freeenv(env, tmp, NULL));
-	ft_freeenv(tmp, env, (*str));
+		return (ft_free_threestr(env, tmp, NULL));
+	ft_free_threestr(tmp, env, (*str));
 	(*str) = result;
+	return (0);
+}
+
+static int	ft_env_double_quotes(char **str, t_envlst *enviroment, int *i)
+{
+	int	j;
+
+	(*i)++;
+	while ((*str)[(*i)] && (*str)[(*i)] != '"')
+	{
+		if ((*str)[(*i)] == '$')
+		{
+			j = (*i);
+			if (ft_get_env(str, (*i), j, enviroment))
+				return (1);
+		}
+		if ((*str)[(*i)] != '\0')
+			(*i)++;
+	}			
 	return (0);
 }
 
 int	ft_look_for_env(char **str, t_envlst *enviroment)
 {
 	int		i;
-	int		j;
 
 	i = 0;
 	while ((*str)[i])
 	{
-		while ((*str)[i] && (*str)[i] != '$' && (*str)[i] != '\'' && (*str)[i] != '"')
-			i++;
 		if ((*str)[i] == '\'')
 		{
 			i++;
@@ -105,24 +111,11 @@ int	ft_look_for_env(char **str, t_envlst *enviroment)
 				i++;
 		}
 		if ((*str)[i] == '"')
-		{
-			i++;
-			while ((*str)[i] && (*str)[i] != '"')
-			{
-				if ((*str)[i] == '$')
-				{
-					j = i;
-					if (ft_get_env(str, i, j, enviroment))
-						return (1);
-				}
-				if ((*str)[i] != '\0')
-					i++;
-			}			
-		}
+			if (ft_env_double_quotes(str, enviroment, &i))
+				return (1);
 		if ((*str)[i] == '$')
 		{
-			j = i;
-			if (ft_get_env(str, i, j, enviroment))
+			if (ft_get_env(str, i, i, enviroment))
 				return (1);
 		}
 		if ((*str)[i] != '\0')
