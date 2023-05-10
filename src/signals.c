28274e6e623a345f98fb05a4773d3bb2a04ff30c
Raphael Rupp <rrupp@c4r2p6.42vienna.com>
2023-05-10 18:09:07 +0200

    i changed some stuff in the signals! :)

--- src/signals.c
+++ src/signals.c
@@ -6,7 +6,7 @@
 /*   By: rrupp <rrupp@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 09:58:05 by rrupp             #+#    #+#             */
-/*   Updated: 2023/05/10 10:16:11 by rrupp            ###   ########.fr       */
+/*   Updated: 2023/05/10 18:07:07 by rrupp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,26 +29,19 @@ void	ft_handle_exe(int signum)
 	if (signum == SIGINT)
 	{
 		write(1, "\n", 1);
+		g_error = 130;
 	}
 }
 
 int	ft_switch_signals(int sig_case)
 {
-	// struct sigaction	sa;
-
 	if (sig_case == INTERACTIV)
 	{
-		// sa.sa_handler = &ft_handle_inter;
-		// if (sigaction(SIGINT, &sa, NULL))
-		// 	return (1);
 		signal(SIGINT, &ft_handle_inter);
 		signal(SIGQUIT, SIG_IGN);
 	}
 	else if (sig_case == EXECUTING)
 	{
-		// sa.sa_handler = &ft_handle_exe;
-		// if (sigaction(SIGINT, &sa, NULL))
-		// 	return (1);
 		signal(SIGINT, &ft_handle_exe);
 		signal(SIGQUIT, SIG_IGN);
 	}
